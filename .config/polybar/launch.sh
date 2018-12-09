b4b17b1d7727e0844d952f5bbefb41d9aee392d1
null <null>
2018-12-09 11:57:56 -0500

    Restructure files into .config folder, add polybar configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# To prevent conflicts, before running ensure that all instances
+# of polybar are first stopped.
+
+killall -q polybar
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+polybar kalibar &
