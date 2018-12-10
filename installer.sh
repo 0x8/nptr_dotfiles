06d8fd1cb91d4722181fd1f9ee7e1601cc9cafbe
null <null>
2018-12-10 12:27:45 -0500

    Added color output to echo commands

--- installer.sh
+++ installer.sh
@@ -21,8 +21,8 @@ info="\x1b[1;37m[INFO]\x1b[0m"
 warn="\x1b[1;35m[WARN]\x1b[0m"
 error="\x1b[1;31m[ERROR]\x1b[0m"
 
-echo "$info Started dotfile installer."
-echo "$info Backups can be found at $BACKUP_LOCATION"
+echo -e  "$info Started dotfile installer."
+echo -e  "$info Backups can be found at $BACKUP_LOCATION"
 
 # Link loose files
 for file in $(ls -a $path_to_self)
@@ -37,18 +37,18 @@ do
         if [ -f "$dest" ]
         then
             # backup existing
-            echo "$info Found existing [$file]. Creating backup."
+            echo -e  "$info Found existing [$file]. Creating backup."
             mv "$dest" "$BACKUP_LOCATION"
         
         elif [ -h "$dest" ]
         then
             # Remove existing symlink
-            echo "$warn Found existing [$file] symlink. Removing."
+            echo -e  "$warn Found existing [$file] symlink. Removing."
             rm "$dest"
         fi
 
         # Make the new link
-        echo "$info Linking [$src] to [$dest]"
+        echo -e  "$info Linking [$src] to [$dest]"
         ln -s $src $dest 
     fi 
 done
@@ -58,17 +58,17 @@ done
 if [ -d "$HOME/.backgrounds" ]
 then
     # backup backgrounds dir
-    echo "$warn Found existing .backgrounds dir. Creating backup."
+    echo -e  "$warn Found existing .backgrounds dir. Creating backup."
     mv "$HOME/.backgrounds" "$BACKUP_LOCATION/.backgrounds"
 
 elif [ -h "$HOME/.backgrounds" ]
 then
     # Remove existing link
-    echo "$warn Found existing .backgrounds symlink. Removing."
+    echo -e  "$warn Found existing .backgrounds symlink. Removing."
     rm "$HOME/.backgrounds"
 fi
 # Make new link
-echo "$info Linking [$path_to_self/backgrounds] to [$HOME/.backgrounds]"
+echo -e  "$info Linking [$path_to_self/backgrounds] to [$HOME/.backgrounds]"
 ln -s "$path_to_self/backgrounds" "$HOME/.backgrounds"
 
 
@@ -80,19 +80,19 @@ do
     if [ -d "$dest" ]
     then
         # Exists as dir in home, make a backup
-        echo "$info Found existing [.config/$dir] at [$dest]. Creating backup."
+        echo -e  "$info Found existing [.config/$dir] at [$dest]. Creating backup."
         mv $dest $BACKUP_LOCATION
 
     elif [ -h "$dest" ]
     then
         # destination is a symlink, we can just remove and overwrite it
         # without harming the original file.
-        echo "$warn Found existing symlink to [.config/$dir]. Removing."
+        echo -e  "$warn Found existing symlink to [.config/$dir]. Removing."
         rm "$dest"
     fi
     
     # Make the new link
-    echo "$info Linking [$src] to [$dest]"
+    echo -e  "$info Linking [$src] to [$dest]"
     ln -s "$src" "$dest"
 done
 
@@ -102,7 +102,7 @@ done
 # -- Ensure backup location exists with proper subdirs
 if [ ! -d $BACKUP_LOCATION/binaries ]
 then
-    echo "$info Did not find a backup subdirectory for binaries. Creating one."
+    echo -e  "$info Did not find a backup subdirectory for binaries. Creating one."
     mkdir -p $BACKUP_LOCATION/binaries
 fi
 # -- Iterate through the binaries
@@ -112,16 +112,16 @@ do
     dest="/usr/local/bin/$binary"
     if [ -f "$dest" ]
     then
-        echo "$info Found existing [$binary] in [$dest]. Creating backup"
+        echo -e  "$info Found existing [$binary] in [$dest]. Creating backup"
         sudo cp "$dest" "$BACKUP_LOCATION/binaries"
 
     elif [ -h "$dest" ]
     then
-        echo "$warn Found existing $dest symlink. Removing."
+        echo -e  "$warn Found existing $dest symlink. Removing."
         sudo rm "$dest"
     fi
 
-    echo "$info Linking [$src] to [$dest]"
+    echo -e  "$info Linking [$src] to [$dest]"
     sudo ln -s $path_to_self/binaries/$binary /usr/local/bin/$binary
 done
 
