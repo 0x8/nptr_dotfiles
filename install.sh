a32fed164171d52ada6528273354cdd4afd55803
nullp0inter <none>
2019-02-06 20:19:25 -0500

    Add gitconfig creator to installer

--- install.sh
+++ install.sh
@@ -158,3 +158,19 @@ else
     echo -e "$warn Failed to find vim autoload dir, pathogen not installed"
     sed -i "s/^execute path/\"execute path/g" $HOME/.vimrc
 fi
+
+
+# Write a simple gitconfig based on env-vars
+GCONF="$HOME/.gitconfig"
+if [ -e "$HOME/.gitconfig" ] # Handle existing config
+then
+    # Make a backup of the existing config
+    mv "$HOME/.gitconfig" "$BACKUP_LOCATION/.gitconfig_backup"
+fi
+# Now can write the new file
+echo "[User]" >> "$GCONF"
+echo "    name = $USER" >> "$GCONF"
+echo "    email = $USER@$HOST" >> "$GCONF"
+echo "[Core]" >> "$GCONF"
+echo "    editor = vim" >> "$GCONF"
+
